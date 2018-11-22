a18bf4c4dd5411882fd1842d4729f15aba77ff9f
maksimmanzhai <bender23max@gmail.com>
2018-11-22 21:53:22 +0300

    Create products

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_11_21_182153) do
+ActiveRecord::Schema.define(version: 2018_11_21_185119) do
 
   create_table "products", force: :cascade do |t|
     t.string "title"
