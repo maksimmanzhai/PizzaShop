68e100e9dc6455c39d941044bcd5639b4e5e5331
maksimmanzhai <bender23max@gmail.com>
2018-11-25 21:46:33 +0300

    Pass id to add_to_cart

--- public/app.js
+++ public/app.js
@@ -1,14 +1,12 @@
 function something() 
 {
 	var x = window.localStorage.getItem('bbb');
-	
 	x = x * 1 + 1;
-
 	window.localStorage.setItem('bbb', x);
-
 	alert(x);
 }
 
-function add_to_cart() {
-	alert('Hello from function!');
+function add_to_cart(id) 
+{
+	alert('You added with id: ' + id);
 }
