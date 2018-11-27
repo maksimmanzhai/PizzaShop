cc8ee734edf96e1abc7f62bc85790a61fe2c9234
maksimmanzhai <bender23max@gmail.com>
2018-11-28 01:01:12 +0300

    Update add_to_cart function

--- public/app.js
+++ public/app.js
@@ -1,12 +1,15 @@
 function something() 
 {
-	var x = window.localStorage.getItem('bbb');
-	x = x * 1 + 1;
-	window.localStorage.setItem('bbb', x);
+	var x = window.localStorage.getItem('bbb'); 	// x = hh['bbb']
+	x = x * 1 + 1; 																// x = x + 1
+	window.localStorage.setItem('bbb', x);				// hh['bbb'] = x
 	alert(x);
 }
 
 function add_to_cart(id) 
 {
-	alert('You added with id: ' + id);
+	var key = 'product_' + id
+	var x = window.localStorage.getItem(key);
+	x = x * 1 + 1;
+	window.localStorage.setItem(key, x);
 }
