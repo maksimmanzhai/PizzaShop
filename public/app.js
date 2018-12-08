d7a3f6c4aafb4663cf5e1a102a686daaa4f24ad0
maksimmanzhai <bender23max@gmail.com>
2018-12-08 13:38:43 +0300

    Add new line to end each file

--- public/app.js
+++ public/app.js
@@ -52,7 +52,3 @@ function update_orders_button()
 	 var text = 'Cart(' + cart_get_number_of_items() + ')';
 	  $('#orders_button').val(text);
 }
-
-function function_name(argument) {
-	// body...
-}
