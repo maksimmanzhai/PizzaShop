1cbf9b81721897377471cc8922b5d0f76fd8a0b2
maksimmanzhai <bender23max@gmail.com>
2018-12-12 22:29:43 +0300

    Add function parse_orders_line

--- app.rb
+++ app.rb
@@ -7,13 +7,7 @@ require 'sinatra/activerecord'
 set :database, "sqlite3:pizzashop.db"
 
 class Product < ActiveRecord::Base
-
 end
-
-# class Order < ActiveRecord::Base
-# 	validates :name, presence: true, length: { minimum: 3}
-# 	validates :phone, presence: true, length: { minimum: 3}
-# end
 
 get '/' do
   @products = Product.all
@@ -24,17 +18,26 @@ get '/about' do
   erb :about
 end
 
-get '/cart' do
-  @c = Order.new
-  erb :cart
+post '/cart' do
+	orders_input = params[:orders_input]
+  erb "hello #{orders_input}"
 end
 
-post '/cart' do
-  @c = Order.new params[:order]
-  if @c.save
-    erb 'Спасибо за ваш заказ!'
-  else
-    @error = @c.errors.full_messages.first
-    erb :cart
-  end
+def parse_orders_line orders_input
+
+	s1 = orders_input.split(/,/)
+
+	arr = []
+
+	s1.each do |x|
+		s2 = x.split(/=/)
+		s3 = s2[0].split(/_/)
+		id = s3[1]
+		cnt = s2[1]
+		arr2 = [id, cnt]
+		arr.push arr2
+	end
+
+	return arr
+
 end
