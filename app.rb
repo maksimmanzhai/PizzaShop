7e6b1995d8132f7e9e2d008874ca3e059471a6bc
maksimmanzhai <bender23max@gmail.com>
2018-12-19 16:25:55 +0300

    Add table to cart view

--- app.rb
+++ app.rb
@@ -19,32 +19,34 @@ get '/about' do
 end
 
 post '/cart' do
-	orders_input = params[:orders]
+	@orders_input = params[:orderstring]
+  @items = parse_orders_input @orders_input
 
-	@items = parse_orders_input orders_input
+  @items.each do |item|
+    # id, cnt
+    item[0] = Product.find(item[0])
+  end
 
-	@items.each do |item|
-		item[0] = Product.find(item[0])
-	end
-
-  erb :cart
+	erb :cart
 end
 
 def parse_orders_input orders_input
+ s1 = orders_input.split(/,/)
 
-	s1 = orders_input.split(/,/)
+  arr = []
 
-	arr = []
+  s1.each do |x|
+    s2 = x.split(/\=/)
 
-	s1.each do |x|
-		s2 = x.split(/\=/)
-		s3 = s2[0].split(/_/)
-		id = s3[1]
-		cnt = s2[1]
-		arr2 = [id, cnt]
-		arr.push arr2
-	end
+    s3 = s2[0].split(/_/)
 
-	return arr
+    id = s3[1]
+    cnt = s2[1]
 
+    arr2 = [id, cnt]
+
+    arr.push arr2
+  end
+
+  return arr
 end
