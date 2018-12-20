6359c9de8023670e21e40fb744e48b28aa77800a
maksimmanzhai <bender23max@gmail.com>
2018-12-20 22:23:15 +0300

    add migration of orders

--- app.rb
+++ app.rb
@@ -1,12 +1,19 @@
-#encoding: utf-8
 require 'rubygems'
 require 'sinatra'
 require 'sinatra/reloader'
 require 'sinatra/activerecord'
 
+set :bind, '0.0.0.0'
 set :database, "sqlite3:pizzashop.db"
 
 class Product < ActiveRecord::Base
+end
+
+class Shipment < ActiveRecord::Base
+  validates :scope, presence: true
+  validates :name, presence: true
+  validates :phone, presence: true
+  validates :address, presence: true
 end
 
 get '/' do
@@ -18,20 +25,45 @@ get '/about' do
   erb :about
 end
 
+get '/contacts' do
+  erb "Under construction"
+end
+
 post '/cart' do
-	@orders_input = params[:orderstring]
+  @orders_input = params[:orderstring]
   @items = parse_orders_input @orders_input
+
+  # выводим сообщение о том, что корзина пуста
+  # if @items.length == 0
+  #   return erb :cart_is_empty
+  # end
 
   @items.each do |item|
     # id, cnt
     item[0] = Product.find(item[0])
   end
 
-	erb :cart
+  erb :cart
+end
+
+get '/admin' do
+   @shipms = Shipment.all
+  erb :orders_list
+end
+
+post '/place_order' do
+  @shipment = Shipment.new params[:order]
+
+  if @shipment.save
+    erb :order_placed
+  else
+    @error = @shipment.errors.full_messages.first
+    erb "Error"
+  end
 end
 
 def parse_orders_input orders_input
- s1 = orders_input.split(/,/)
+  s1 = orders_input.split(/,/)
 
   arr = []
 
--- db/migrate/20181220184947_create_orders.rb
+++ db/migrate/20181220184947_create_orders.rb
@@ -1,11 +1,13 @@
-class CreateOrders < ActiveRecord::Migration[5.2]
+class AddOrders < ActiveRecord::Migration[5.2]
   def change
   	create_table :orders do |t| #4. greate table
+			t.text :orders_input
 			t.text :name
 			t.text :phone
-			t.text :list
-
+			t.text :address
+			
 			t.timestamps
 		end #5. rake db:migrate
   end
 end
+
