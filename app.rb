70f753477dae98bbfa65b5f5a430b32937e3d317
maksimmanzhai <bender23max@gmail.com>
2018-12-21 19:30:15 +0300

    Add cancel button and stu for js handler

--- app.rb
+++ app.rb
@@ -30,7 +30,7 @@ get '/contacts' do
 end
 
 post '/cart' do
-  @orders_input = params[:orders]
+  @orders_input = params[:orders_input]
   @items = parse_orders_input @orders_input
 
   # выводим сообщение о том, что корзина пуста
--- public/app.js
+++ public/app.js
@@ -1,6 +1,7 @@
-function add_to_cart(id) 
+function add_to_cart(id)
 {
-	var key = 'product_' + id
+	var key = 'product_' + id;
+
 	var x = window.localStorage.getItem(key);
 	x = x * 1 + 1;
 	window.localStorage.setItem(key, x);
@@ -9,59 +10,63 @@ function add_to_cart(id)
 	update_orders_button();
 }
 
-function cart_get_number_of_items() 
+function update_orders_input()
 {
+	var orders = cart_get_orders();
+	$('#orders_input').val(orders);
+}
 
-	var text = "";
-	var i;
-	for (var i = 0, len = localStorage.length; i < len; i++)
-	{
-		text = text + localStorage.key(i) + " => " + localStorage.getItem(localStorage.key(i)) + "; ";
+function update_orders_button()
+{
+	var text = 'Cart (' + cart_get_number_of_items() + ')';
+	$('#orders_button').val(text);
+}
 
+function cart_get_number_of_items()
+{
 	var cnt = 0;
 
-	for (var i = 0; i < window.localStorage.length; i++) 
+	for(var i = 0; i < window.localStorage.length; i++)
 	{
-		var key = window.localStorage.key(i); // getting key
-		var value = window.localStorage.getItem(key); // getting value, analog from ruby hh[key] = x
-		if (key.indexOf('product_') == 0)
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
 		{
 			cnt = cnt + value * 1;
 		}
-
 	}
-	alert(text);
-	
+
+	return cnt;
 }
 
-function cart_get_orders() 
+function cart_get_orders()
 {
-	var orders = 0;
+	var orders = '';
 
-	for (var i = 0; i < window.localStorage.length; i++) 
+	for(var i = 0; i < window.localStorage.length; i++)
 	{
-		var key = window.localStorage.key(i); // getting key
-		var value = window.localStorage.getItem(key); // getting value, analog from ruby hh[key] = x
-		if (key.indexOf('product_') == 0)
+		var key = window.localStorage.key(i); // получаем ключ
+		var value = window.localStorage.getItem(key); // получаем значение, аналог в ruby: hh[key] = x
+
+		if(key.indexOf('product_') == 0)
 		{
 			orders = orders + key + '=' + value + ',';
 		}
 	}
+
 	return orders;
 }
 
-function update_orders_input() 
+function cancel_order()
 {
-	 var orders = cart_get_orders();
-	 $('#orders_input').val(orders);
+	window.localStorage.clear();
+
+	update_orders_input();
+	update_orders_button();
+
+	$('#cart').text('Your cart is now empty');
+
+	return false;	
 }
 
-function update_orders_button() 
-{
-	 var text = 'Cart(' + cart_get_number_of_items() + ')';
-	  $('#orders_button').val(text);
-}
-
-function function_name(argument) {
-	// body...
-}
