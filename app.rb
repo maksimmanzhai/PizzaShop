c371df1615190d0dcaf081995ad839fa7ace7f30
maksimmanzhai <bender23max@gmail.com>
2018-12-21 21:35:13 +0300

    End of lesson 36

--- app.rb
+++ app.rb
@@ -34,9 +34,9 @@ post '/cart' do
   @items = parse_orders_input @orders_input
 
   # выводим сообщение о том, что корзина пуста
-  # if @items.length == 0
-  #   return erb :cart_is_empty
-  # end
+  if @items.length == 0
+    return erb :cart_is_empty
+  end
 
   @items.each do |item|
     # id, cnt
